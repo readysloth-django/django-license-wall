90bccdc24967823db0bd86e825dcb9dab1e048e8
readysloth <readysloth@protonmail.com>
2024-03-24 12:29:05 +0300

    feat(django_license_wall/models.py): Added model for saving `LicenseKey`

--- django_license_wall/models.py
+++ django_license_wall/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from solo.models import SingletonModel
+
+
+class LicenseKey(SingletonModel):
+    key = models.CharField(_('Key'), max_length=32)
+
+    class Meta:
+        verbose_name = _('License key')
+        verbose_name_plural = _('License keys')
+
+    def __str__(self):
+        return self.key
